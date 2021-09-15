f85c900cae4970128a72e6765e7975aa95dc61d1
Marco Solis <marcosolis@192.168.1.3>
2021-09-15 16:35:31 -0300

    Transformando o botão em um componente

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './Home.scss';
 
 import Header from '../../components/Header/Header';
+import Button from '../../components/Button/Button';
 import BannerImage from '../../assets/images/Home_banner-cover-image.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
@@ -29,108 +30,10 @@ export default function Home() {
 							<h3 className="tituloProduto">nome do produto</h3>
 							<p className="precoAntigo">R$ 899,00</p>
 							<p className="precoProduto">R$ 799,99</p>
-							<button>
+							<Button>
 								<FontAwesomeIcon icon={faCartPlus} />
 								adicionar ao carrinho
-							</button>
-						</div>
-					</div>
-					<div className="produto">
-						<div className="imagemProduto">
-							<img src="#" alt="" />
-						</div>
-						<div className="textoProduto">
-							<h3 className="tituloProduto">nome do produto</h3>
-							<p className="precoAntigo">R$ 899,00</p>
-							<p className="precoProduto">R$ 799,99</p>
-							<button>
-								<FontAwesomeIcon icon={faCartPlus} />
-								adicionar ao carrinho
-							</button>
-						</div>
-					</div>
-					<div className="produto">
-						<div className="imagemProduto">
-							<img src="#" alt="" />
-						</div>
-						<div className="textoProduto">
-							<h3 className="tituloProduto">nome do produto</h3>
-							<p className="precoAntigo">R$ 899,00</p>
-							<p className="precoProduto">R$ 799,99</p>
-							<button>
-								<FontAwesomeIcon icon={faCartPlus} />
-								adicionar ao carrinho
-							</button>
-						</div>
-					</div>
-					<div className="produto">
-						<div className="imagemProduto">
-							<img src="#" alt="" />
-						</div>
-						<div className="textoProduto">
-							<h3 className="tituloProduto">nome do produto</h3>
-							<p className="precoAntigo">R$ 899,00</p>
-							<p className="precoProduto">R$ 799,99</p>
-							<button>
-								<FontAwesomeIcon icon={faCartPlus} />
-								adicionar ao carrinho
-							</button>
-						</div>
-					</div>
-					<div className="produto">
-						<div className="imagemProduto">
-							<img src="#" alt="" />
-						</div>
-						<div className="textoProduto">
-							<h3 className="tituloProduto">nome do produto</h3>
-							<p className="precoAntigo">R$ 899,00</p>
-							<p className="precoProduto">R$ 799,99</p>
-							<button>
-								<FontAwesomeIcon icon={faCartPlus} />
-								adicionar ao carrinho
-							</button>
-						</div>
-					</div>
-					<div className="produto">
-						<div className="imagemProduto">
-							<img src="#" alt="" />
-						</div>
-						<div className="textoProduto">
-							<h3 className="tituloProduto">nome do produto</h3>
-							<p className="precoAntigo">R$ 899,00</p>
-							<p className="precoProduto">R$ 799,99</p>
-							<button>
-								<FontAwesomeIcon icon={faCartPlus} />
-								adicionar ao carrinho
-							</button>
-						</div>
-					</div>
-					<div className="produto">
-						<div className="imagemProduto">
-							<img src="#" alt="" />
-						</div>
-						<div className="textoProduto">
-							<h3 className="tituloProduto">nome do produto</h3>
-							<p className="precoAntigo">R$ 899,00</p>
-							<p className="precoProduto">R$ 799,99</p>
-							<button>
-								<FontAwesomeIcon icon={faCartPlus} />
-								adicionar ao carrinho
-							</button>
-						</div>
-					</div>
-					<div className="produto">
-						<div className="imagemProduto">
-							<img src="#" alt="" />
-						</div>
-						<div className="textoProduto">
-							<h3 className="tituloProduto">nome do produto</h3>
-							<p className="precoAntigo">R$ 899,00</p>
-							<p className="precoProduto">R$ 799,99</p>
-							<button>
-								<FontAwesomeIcon icon={faCartPlus} />
-								adicionar ao carrinho
-							</button>
+							</Button>
 						</div>
 					</div>
 				</div>
