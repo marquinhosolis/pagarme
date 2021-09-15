731f0fd3170240ee6fc6d9409caec20983b9967d
Marco Solis <marcosolis@192.168.1.3>
2021-09-15 16:27:21 -0300

    Vitrine Home HTML

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './globals.scss';
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,6 +4,8 @@ import './Home.scss';
 
 import Header from '../../components/Header';
 import BannerImage from '../../assets/images/Home_banner-cover-image.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home() {
 	return (
@@ -17,7 +19,122 @@ export default function Home() {
 					<img src={BannerImage} alt="Mulher com sacolas na mão" />
 				</div>
 			</div>
-			Home
+			<div className="vitrineProdutos">
+				<div className="container">
+					<div className="produto">
+						<div className="imagemProduto">
+							<img src="#" alt="" />
+						</div>
+						<div className="textoProduto">
+							<h3 className="tituloProduto">nome do produto</h3>
+							<p className="precoAntigo">R$ 899,00</p>
+							<p className="precoProduto">R$ 799,99</p>
+							<button>
+								<FontAwesomeIcon icon={faCartPlus} />
+								adicionar ao carrinho
+							</button>
+						</div>
+					</div>
+					<div className="produto">
+						<div className="imagemProduto">
+							<img src="#" alt="" />
+						</div>
+						<div className="textoProduto">
+							<h3 className="tituloProduto">nome do produto</h3>
+							<p className="precoAntigo">R$ 899,00</p>
+							<p className="precoProduto">R$ 799,99</p>
+							<button>
+								<FontAwesomeIcon icon={faCartPlus} />
+								adicionar ao carrinho
+							</button>
+						</div>
+					</div>
+					<div className="produto">
+						<div className="imagemProduto">
+							<img src="#" alt="" />
+						</div>
+						<div className="textoProduto">
+							<h3 className="tituloProduto">nome do produto</h3>
+							<p className="precoAntigo">R$ 899,00</p>
+							<p className="precoProduto">R$ 799,99</p>
+							<button>
+								<FontAwesomeIcon icon={faCartPlus} />
+								adicionar ao carrinho
+							</button>
+						</div>
+					</div>
+					<div className="produto">
+						<div className="imagemProduto">
+							<img src="#" alt="" />
+						</div>
+						<div className="textoProduto">
+							<h3 className="tituloProduto">nome do produto</h3>
+							<p className="precoAntigo">R$ 899,00</p>
+							<p className="precoProduto">R$ 799,99</p>
+							<button>
+								<FontAwesomeIcon icon={faCartPlus} />
+								adicionar ao carrinho
+							</button>
+						</div>
+					</div>
+					<div className="produto">
+						<div className="imagemProduto">
+							<img src="#" alt="" />
+						</div>
+						<div className="textoProduto">
+							<h3 className="tituloProduto">nome do produto</h3>
+							<p className="precoAntigo">R$ 899,00</p>
+							<p className="precoProduto">R$ 799,99</p>
+							<button>
+								<FontAwesomeIcon icon={faCartPlus} />
+								adicionar ao carrinho
+							</button>
+						</div>
+					</div>
+					<div className="produto">
+						<div className="imagemProduto">
+							<img src="#" alt="" />
+						</div>
+						<div className="textoProduto">
+							<h3 className="tituloProduto">nome do produto</h3>
+							<p className="precoAntigo">R$ 899,00</p>
+							<p className="precoProduto">R$ 799,99</p>
+							<button>
+								<FontAwesomeIcon icon={faCartPlus} />
+								adicionar ao carrinho
+							</button>
+						</div>
+					</div>
+					<div className="produto">
+						<div className="imagemProduto">
+							<img src="#" alt="" />
+						</div>
+						<div className="textoProduto">
+							<h3 className="tituloProduto">nome do produto</h3>
+							<p className="precoAntigo">R$ 899,00</p>
+							<p className="precoProduto">R$ 799,99</p>
+							<button>
+								<FontAwesomeIcon icon={faCartPlus} />
+								adicionar ao carrinho
+							</button>
+						</div>
+					</div>
+					<div className="produto">
+						<div className="imagemProduto">
+							<img src="#" alt="" />
+						</div>
+						<div className="textoProduto">
+							<h3 className="tituloProduto">nome do produto</h3>
+							<p className="precoAntigo">R$ 899,00</p>
+							<p className="precoProduto">R$ 799,99</p>
+							<button>
+								<FontAwesomeIcon icon={faCartPlus} />
+								adicionar ao carrinho
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
