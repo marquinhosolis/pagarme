e794a347d1354965c04dae13c0eeeea1dd52f84c
Marco Solis <marcosolis@192.168.1.3>
2021-09-15 16:21:06 -0300

    Inserindo banner principal na Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import './Home.scss';
+
 import Header from '../../components/Header';
+import BannerImage from '../../assets/images/Home_banner-cover-image.png';
 
 export default function Home() {
 	return (
 		<>
 			<Header />
+			<div className="bannerCover">
+				<div className="container">
+					<h1>
+						<span>15%OFF</span>em produtos selecionados
+					</h1>
+					<img src={BannerImage} alt="Mulher com sacolas na mão" />
+				</div>
+			</div>
 			Home
 		</>
 	);
