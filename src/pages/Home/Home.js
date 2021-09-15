fc673144913c11c5732295677898e773110ba84a
Marco Solis <marcosolis@192.168.1.3>
2021-09-15 15:52:36 -0300

    Adicionando Biblioteca Fontawesome

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlayCircle } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home() {
 	return <div className>Home</div>;
